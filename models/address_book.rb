21b1b2b5218ab9e7735cb3b7221d062d09ee1d91
luischernandez <luishernandez01@live.com>
2017-11-11 22:42:01 -0700

    Create iterative search

--- models/address_book.rb
+++ models/address_book.rb
@@ -62,4 +62,15 @@ class AddressBook
 
         return nil
     end
+
+    def iterative_search(name)
+        entries.each do |item|
+            if name == item.name
+                #puts item
+                return item
+            end
+        end
+            return nil
+    end
+
 end
